4f5a033b84cd76585735c32449b166cdd1f2b3f1
Furkan Gözükara <furkangozukara@gmail.com>
2019-10-22 10:22:07 +0300

    lecture 7 v.3

---
+++ lectures/lecture 7 generic specific code/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_7_generic_specific_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int is the type argument
+            GenericList<int> list = new GenericList<int>();
+
+            for (int x = 0; x < 10; x++)
+            {
+                list.AddHead(x);
+            }
+
+            foreach (int i in list)
+            {
+                System.Console.Write(i + " ");
+            }
+            System.Console.WriteLine("\nDone");
+            Console.ReadLine();
+        }
+
+        // type parameter T in angle brackets
+        public class GenericList<T>
+        {
+            // The nested class is also generic on T.
+            private class Node
+            {
+                // T used in non-generic constructor.
+                public Node(T t)
+                {
+                    next = null;
+                    data = t;
+                }
+
+                private Node next;
+                public Node Next
+                {
+                    get { return next; }
+                    set { next = value; }
+                }
+
+                // T as private member data type.
+                private T data;
+
+                // T as return type of property.
+                public T Data
+                {
+                    get { return data; }
+                    set { data = value; }
+                }
+            }
+
+            private Node head;
+
+            // constructor
+            public GenericList()
+            {
+                head = null;
+            }
+
+            // T as method parameter type:
+            public void AddHead(T t)
+            {
+                Node n = new Node(t);
+                n.Next = head;
+                head = n;
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                Node current = head;
+
+                while (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+        }
+    }
+}
