d0b09cfff1ad16475dfcb33e0fe250e19a3cf5cd
Furkan Gözükara <furkangozukara@gmail.com>
2019-12-03 11:27:22 +0300

    lecture 13 v2

---
+++ lectures/lecture 13 asyn await/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_13_asyn_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            callMethod();
+            Console.ReadKey();
+        }
+
+        public static async void callMethod()
+        {
+            Task<int> task = Method1();
+            Method2();
+            int count = await task;
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    count += 1;
+                }
+            });
+            return count;
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            Console.WriteLine("Total count is " + count);
+        }
+    }
+}
--- lectures/lecture 13/MainWindow.xaml.cs
+++ lectures/lecture 13/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -44,30 +45,102 @@ namespace lecture_13
 
         private void BtnModify_Click(object sender, RoutedEventArgs e)
         {
-            Task.Factory.StartNew(() => {
+            Task.Factory.StartNew(() =>
+            {
                 for (int i = 0; i < Int32.MaxValue; i++)
                 {
                     Task.Factory.StartNew(() => { incNums(); });
                 }
             });
-         
+
         }
 
         static int irNumber4;
+        private static object objlock = new object();
 
         private void incNums()
         {
-            irNumber4++;
-            Interlocked.Increment(ref realValue);
-            myExample.number1++;
-            myExample.number2++;
-            myExample.number3++;
+            lock (objlock)
+            {
+                irNumber4++;
+                Interlocked.Increment(ref realValue);
+                myExample.number1++;
+                myExample.number2++;
+                myExample.number3++;
+            }
         }
 
         private void BtnPrint_Click(object sender, RoutedEventArgs e)
         {
-            string srMsg = $"val 1 : {myExample.number1.ToString("N0")} \t val 2 : {myExample.number2.ToString("N0")} \t val 3 : {myExample.number3.ToString("N0")} \t val 4 : {irNumber4.ToString("N0")} \t real value : {Interlocked.Read(ref realValue).ToString("N0")}";
+            string srMsg = "";
+            lock (objlock)
+            {
+                srMsg = $"val 1 : {myExample.number1.ToString("N0")} \t val 2 : {myExample.number2.ToString("N0")} \t val 3 : {myExample.number3.ToString("N0")} \t val 4 : {irNumber4.ToString("N0")} \t real value : {Interlocked.Read(ref realValue).ToString("N0")}";
+            }
             lblScreen.Content = srMsg;
+        }
+
+        private void BtnUsingExample_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Task.Factory.StartNew(() => { taskDownload("https://www.memurlar.net?" + i); });
+            }
+        }
+
+        private void taskDownload(string srUrl)
+        {
+            using (WebClient wb = new WebClient())
+            {
+                string srSource = wb.DownloadString(srUrl);
+            }
+        }
+
+        static Dictionary<string, int> dicValues = new Dictionary<string, int>();
+
+        private void BtnConqDic_Click(object sender, RoutedEventArgs e)
+        {
+            Random randGenerator = new Random();
+
+            while (true)
+            {
+                //   Task.Factory.StartNew(() => { addDic(randGenerator.Next(0, 100000).ToString()); });
+                Task.Factory.StartNew(() => { addDicConc(randGenerator.Next(0, 100000).ToString()); });
+            }
+        }
+
+        private void addDic(string srNumber)
+        {
+            //this gives error
+            if (!dicValues.ContainsKey(srNumber))
+            {
+                dicValues.Add(srNumber, 1);
+            }
+            else
+                dicValues[srNumber]++;
+        }
+
+        ConcurrentDictionary<string, int> dicConc = new ConcurrentDictionary<string, int>();
+
+        private void addDicConc(string srNumber)
+        {
+            //this gives error
+            if (!dicConc.ContainsKey(srNumber))
+            {
+                bool blResult = dicConc.TryAdd(srNumber, 1);
+                if (blResult == false)
+                    dicConc[srNumber]++;
+            }
+            else
+                dicConc[srNumber]++;
+        }
+
+        static async Task<string> downloadWebPage(string srUrl)
+        {
+            using (WebClient wb = new WebClient())
+            {
+                return wb.DownloadString(srUrl);
+            }
         }
     }
 }
