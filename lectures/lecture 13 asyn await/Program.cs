c6ccfd12019bc465aa0732b9d2f8709945ddd53c
Furkan Gözükara <furkangozukara@gmail.com>
2019-12-03 11:41:28 +0300

    lecture 13 v3

--- lectures/lecture 13 asyn await/Program.cs
+++ lectures/lecture 13 asyn await/Program.cs
@@ -11,16 +11,52 @@ namespace lecture_13_asyn_await
     {
         static void Main(string[] args)
         {
-            callMethod();
+            //callMethod();
+            // callMethod2();
+             callMethod3();
             Console.ReadKey();
+        }
+
+        public static void callMethod3()
+        {
+            int irResult = 0;
+
+            Task.Factory.StartNew(() => { irResult = Method1alt(); }).ContinueWith(pr => Console.WriteLine(irResult) );
+            Method2();
+           
+        }
+
+        public static void callMethod2()
+        {
+            int irResult = 0;
+            var vrTask1 = Task.Factory.StartNew(() => { irResult = Method1alt(); });
+            Method2();
+            Console.Clear();
+            Console.WriteLine(irResult);
+            vrTask1.Wait();
+            Method3(irResult);
+            
         }
 
         public static async void callMethod()
         {
-            Task<int> task = Method1();
-            Method2();
-            int count = await task;
-            Method3(count);
+            Task<int> task = Method1();//this starts and continue. does not wait to finish
+            Method2();//this starts and program waits until it is done
+            int count = await task;//here it waits until method 1 is done
+            Method3(count);//and this executes
+        }
+
+        public static int Method1alt()
+        {
+            int count = 0;
+        
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    count += 1;
+                }
+          
+            return count;
         }
 
         public static async Task<int> Method1()
